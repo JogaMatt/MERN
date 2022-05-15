20dc9497b8fa4a27eabc53b780da77522e7b41ff
DESKTOP-UK2RI9P\Matt Joga <matthewjoga@outlook.com>
2022-05-15 11:56:33 -0400

    commit

--- React/pokemon_api/src/components/Pokemon.jsx
+++ React/pokemon_api/src/components/Pokemon.jsx
@@ -8,9 +8,11 @@ const Pokemon = (props) => {
 
     const [apiState, setAPIState] = useState()
 
+    const [cardState, setCardState] = useState()
+
     const eventHandler = (event) => {
         event.preventDefault()
-        axios.get("https://api.pokemontcg.io/v2/cards?q=name:" + pokemon.replace(" ", "*"))
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=898&offset=0")
             .then(successResponse => {
                 console.log("SUCCESS: ", successResponse.data)
                 setAPIState(successResponse.data)
@@ -18,27 +20,59 @@ const Pokemon = (props) => {
             .catch(errorResponse => console.log("ERROR: ", errorResponse))
     }
 
+    const cardHandler = (event) => {
+        event.preventDefault()
+        axios.get("https://api.pokemontcg.io/v2/cards?q=name:" + pokemon.replace(" ", "*"))
+            .then(successResponse => {
+                console.log("SUCCESS: ", successResponse.data)
+                setCardState(successResponse.data)
+            })
+            .catch(errorResponse => console.log("ERROR: ", errorResponse))
+    }
+
 
   return (
-    <>
-        <form onSubmit={eventHandler} >
-            <input type="text" onChange={(e) => setPokemon(e.target.value)} />
-            <button>Fetch Pokemon</button>
-        </form>
+    <div id='container'>
+        <div className="dex">
+            <form onSubmit={eventHandler} >
+                <button>Fetch Pokemon</button>
+            </form>
         
-        {
-            (apiState) ?
-            <>
-                {
-                    apiState.data.map((pokemon, index) => {
-                        return (
-                            <img key={index}  src={pokemon.images.large} alt="" className='pokemonCard'/>
-                        )
-                    })
-                }
-            </> : <h1>Click the button to see all the Pokemon!</h1>
-        }
-    </>
+            {
+                (apiState) ?
+                <>
+                    {
+                        apiState.results.map((pokemon, index) => {
+                            return (
+                                <li key={index}>{pokemon.name}</li>
+                            )
+                        })
+                    }
+                </> : <h1>Click the button to see all the Pokemon!</h1>
+            }
+        </div>
+        
+        <div className="card">
+            <form onSubmit={cardHandler} >
+                <input type="text" onChange={(e) => setPokemon(e.target.value)} />
+                <button>Fetch Pokemon</button>
+            </form>
+            
+            {
+                (cardState) ?
+                <>
+                    {
+                        cardState.data.map((pokemon, index) => {
+                            return (
+                                <img key={index}  src={pokemon.images.large} alt="" className='pokemonCard'/>
+                            )
+                        })
+                    }
+                </> : null
+            }
+        </div>
+        
+    </div>
   )
 }
 
