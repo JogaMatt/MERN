259d163febcd7538f72c834b83f713f224a6756d
DESKTOP-UK2RI9P\Matt Joga <matthewjoga@outlook.com>
2022-08-26 10:39:24 -0400

    changes

--- Project/client/src/views/MainPage.jsx
+++ Project/client/src/views/MainPage.jsx
@@ -26,31 +26,31 @@ const MainPage = () => {
         <div style={{margin: 'auto', width: 600, marginBottom: 15}} className="bannerImg">
           <img style={{width: 600}} src='https://tcg.pokemon.com/assets/img/expansions/brilliant-stars/global/logos/en-us/swsh09-logo.png' alt="" />
         </div>
-        <Link to={'/swsh9'} style={{textDecoration: 'none'}}><button id='expansionBtn' className='btn btn-dark'><em>Go to the expansion page</em></button></Link>
-        <div className="vstar" style={{display: 'flex', justifyContent: 'center', marginTop: 350}}>
+        <div className="vstar" style={{display: 'flex', justifyContent: 'center', marginTop: 215}}>
           <Link id='mainArceus' to={'/swsh9/swsh9-123'}><img className='arceus' style={{width: 400, borderRadius: 10, marginRight: 100, marginTop: -175}} src="https://tcg.pokemon.com/assets/img/expansions/brilliant-stars/cards/en-us/SWSH09_EN_123.jpg" alt="" /></Link>
           <div className="description" style={{width: 600, marginTop: 0}}>
             <img style={{width: 300, marginLeft: 145}} src="https://tcg.pokemon.com/assets/img/expansions/brilliant-stars/global/logos/en-us/vstar-logo.png" alt="" />
             <div className="divider" style={{width: 'inherit', height: 15 ,backgroundColor: '#fdf4f8'}}></div>
             <h5 style={{color: '#fdf4f8', textAlign: 'center', marginTop: 60}}>The shining stars of this Pokémon TCG expansion are undeniably the Pokémon VSTAR. Each one has a VSTAR Power, a special attack or Ability that can drastically alter the course of a battle. But choose your moment wisely, because each player can only use one VSTAR Power during the entire game!</h5>
+            {/* <Link to={'/swsh9'} style={{textDecoration: 'none'}}><button id='expansionBtn' className='btn btn-dark'><em>Go to the expansion page</em></button></Link> */}
           </div>
         </div>
         <div className="dividerBig" style={{width: 'inherit', height: 30 ,backgroundColor: 'black', marginTop: -45}}></div>
-        <div style={{backgroundColor: '#f6f7f7', height: 1120}} className="trainerGallery">
+        <div style={{backgroundColor: '#f6f7f7'}} className="trainerGallery">
           <div style={{display: 'flex', justifyContent: 'center'}} className="starDivider">
             <img style={{width: 750, marginTop: 40}} src="https://tcg.pokemon.com/assets/img/expansions/brilliant-stars/global/star-divider.png" alt="" />
           </div>
           <div className="trainerInfo">
             <h1 style={{fontSize: 36, color: '#0a1045', fontFamily: 'serif', textAlign: 'center', marginTop: 70, marginBottom: 45, letterSpacing: -2}}><b>TRAINER GALLERY CARDS</b></h1>
             <h5 style={{width: 1200, textAlign: 'center', margin: 'auto', lineHeight: 1.5}}>Celebrate the incredible connection between Trainers and their Pokémon with the Trainer Gallery subset. Among the star Trainers are two of the Pokémon world's favorite rivals—Blue and his electrifying Jolteon, and N and Zekrom. Karen of the Elite Four also puts in an appearance with the mysterious Moonlight Pokémon, Umbreon VMAX.</h5>
-            <div className="cardImg" style={{width: 1080, marginLeft: 747, marginTop: 70}}>
+            <div className="cardImg" style={{width: 1080, marginTop: 70}}>
               <Link to={'/swsh9tg/swsh9tg-TG04'}><img onMouseOver={overBackground} onMouseLeave={leaveBackground} style={{borderRadius: 10, width: 300, marginRight: 50}} src="https://tcg.pokemon.com/assets/img/expansions/brilliant-stars/cards/en-us/SWSH09_EN_TG_4.jpg" alt="" /></Link>
               <Link to={'/swsh9tg/swsh9tg-TG05'}><img onMouseOver={overBackground} onMouseLeave={leaveBackground} style={{borderRadius: 10, width: 300, marginRight: 50}} src="https://tcg.pokemon.com/assets/img/expansions/brilliant-stars/cards/en-us/SWSH09_EN_TG_5.jpg" alt="" /></Link>
               <Link to={'/swsh9tg/swsh9tg-TG23'}><img onMouseOver={overBackground} onMouseLeave={leaveBackground} style={{borderRadius: 10, width: 300}} src="https://tcg.pokemon.com/assets/img/expansions/brilliant-stars/cards/en-us/SWSH09_EN_TG_23.jpg" alt="" /></Link>
             </div>
-            <div className="trainerButton" style={{width: 293, margin: 'auto', marginTop: 45}}>
+            {/* <div className="trainerButton" style={{width: 293, margin: 'auto', marginTop: 45}}>
               <Link to={'/swsh9tg'} style={{textDecoration: 'none'}}><button className='btn btn-dark'><em>Go to the expansion page</em></button></Link>
-            </div>
+            </div> */}
           </div>
         </div>
       </main>
